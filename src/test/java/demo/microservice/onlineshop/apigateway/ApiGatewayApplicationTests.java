38ff06ed2ce5a0ad9bbe5303e6c5c484a6fd45d3
Zayar Linn Naung <zayarlinnnaung.coder@gmail.com>
2023-09-19 23:05:53 +0630

    first commit

---
+++ src/test/java/demo/microservice/onlineshop/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.microservice.onlineshop.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
